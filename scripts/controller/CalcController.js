5916bbf43465fa6f0b3c48931dc0328e129fdcd6
Rafael Oliveira <rafaelbatistacg@gmail.com>
2019-12-03 09:21:45 -0300

    switch creation to handle operations

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor()
     {
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -35,15 +36,80 @@ class CalcController {
 
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+
+        switch (value){
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                break;
+            case 'subtracao':
+                break;
+
+            case 'divisao':
+                break;
+
+            case 'multiplicacao':
+                break;
+
+            case 'porcento':
+                break;
+
+            case 'igual':
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break
+
+            default:
+                this.setError();
+                break;
+        }
+    }
+
+
     initButtonsEvents(){
         // recuperando todos os filhos do id buttons e do id parts
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
         buttons.forEach((btn, index)=>{
 
-            this.addEventListenerAll(btn, 'click drag mouseover', func => {
+            this.addEventListenerAll(btn, 'click drag', func => {
 
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn= btn.className.baseVal.replace("btn-", "");
+                this.execBtn(textBtn);
 
             })
 
@@ -68,11 +134,11 @@ class CalcController {
     }
 
     get displayCalc(){
-        return this._displayCalc.innerHTML;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(newDisplay){
-        this._displayCalc.innerHTML = newDisplay;
+        this._displayCalcEl.innerHTML = newDisplay;
     }
 
     get displayTime(){
